3a848a915b572d6ab3c586a2269f0d2562b407e6
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-10-18 17:13:01 +0200

    homework

---
+++ src/api/blogs/model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const blogsSchema = new Schema(
+  {
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    readTime: {
+      value: { type: Number },
+      unit: { type: Number },
+    },
+    author: {
+      name: { type: String },
+      avatar: { type: String },
+    },
+    content: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("blog", blogsSchema);
